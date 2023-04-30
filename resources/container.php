098f080c46f2d8abb4a297a8932512ea508937b1
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-04-30 10:53:17 +0200

    register engine

--- resources/container.php
+++ resources/container.php
@@ -1,10 +1,12 @@
 <?php
 
+use Fusio\Adapter\Fcgi\Action\FcgiEngine;
 use Fusio\Adapter\Fcgi\Action\FcgiProcessor;
 use Fusio\Engine\Adapter\ServiceBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $container) {
     $services = ServiceBuilder::build($container);
+    $services->set(FcgiEngine::class);
     $services->set(FcgiProcessor::class);
 };
