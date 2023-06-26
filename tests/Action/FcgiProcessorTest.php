5ab6ec81d76941f27b7099f81ec0c8b134f6153a
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-06-26 19:04:05 +0200

    update engine test

--- tests/Action/FcgiProcessorTest.php
+++ tests/Action/FcgiProcessorTest.php
@@ -25,9 +25,8 @@ use Fusio\Adapter\Fcgi\Action\FcgiProcessor;
 use Fusio\Adapter\Fcgi\Tests\FcgiTestCase;
 use Fusio\Engine\Form\Builder;
 use Fusio\Engine\Form\Container;
-use Fusio\Engine\Test\EngineTestCaseTrait;
-use PHPUnit\Framework\TestCase;
 use PSX\Http\Environment\HttpResponseInterface;
+use PSX\Record\Record;
 
 /**
  * FcgiProcessorTest
@@ -78,6 +77,73 @@ JSON;
         $this->assertEquals(200, $response->getStatusCode(), $actual);
         $this->assertEquals(['content-type' => 'application/json'], $response->getHeaders());
         $this->assertJsonStringEqualsJsonString($expect, $actual, $actual);
+    }
+
+    public function testHandlePost()
+    {
+        $this->pingFastCGIServer();
+
+        $action = $this->getActionFactory()->factory(FcgiProcessor::class);
+        $script = realpath(__DIR__ . '/../json.php');
+
+        // handle request
+        $response = $action->handle(
+            $this->getRequest('POST', [], [], [], new Record(['foo' => 'bar'])),
+            $this->getParameters(['host' => '127.0.0.1', 'port' => 9090, 'script' => $script]),
+            $this->getContext()
+        );
+
+        $actual = json_encode($response->getBody(), JSON_PRETTY_PRINT);
+        $script = json_encode($script);
+        $expect = <<<JSON
+{
+    "foo": "bar",
+    "body": {
+        "foo": "bar"
+    },
+    "server": {
+        "REQUEST_METHOD": "POST",
+        "REQUEST_URI": "",
+        "SCRIPT_FILENAME": {$script},
+        "CONTENT_TYPE": "application\/json",
+        "OPERATION_ID": "34",
+        "REMOTE_ADDR": "127.0.0.1",
+        "REMOTE_USER": "Consumer",
+        "USER_ANONYMOUS": "0",
+        "USER_ID": "2",
+        "APP_ID": "3",
+        "APP_KEY": "5347307d-d801-4075-9aaa-a21a29a448c5"
+    }
+}
+JSON;
+
+        $this->assertInstanceOf(HttpResponseInterface::class, $response);
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals(['content-type' => 'application/json'], $response->getHeaders());
+        $this->assertJsonStringEqualsJsonString($expect, $actual, $actual);
+    }
+
+    public function testHandleHtml()
+    {
+        $this->pingFastCGIServer();
+
+        $action = $this->getActionFactory()->factory(FcgiProcessor::class);
+        $script = realpath(__DIR__ . '/../html.php');
+
+        // handle request
+        $response = $action->handle(
+            $this->getRequest('GET'),
+            $this->getParameters(['host' => '127.0.0.1', 'port' => 9090, 'script' => $script]),
+            $this->getContext()
+        );
+
+        $actual = $response->getBody();
+        $expect = 'foobar';
+
+        $this->assertInstanceOf(HttpResponseInterface::class, $response);
+        $this->assertEquals(200, $response->getStatusCode(), $actual);
+        $this->assertEquals(['content-type' => 'text/html;charset=UTF-8'], $response->getHeaders());
+        $this->assertEquals($expect, $actual, $actual);
     }
 
     public function testGetForm()
